05cefe3af3d94b8e0087c694c5d72f797286b4a4
RutujaWanjari <rutuja.wanjari2@gmail.com>
2018-09-17 16:11:02 +0530

    some bug fixes

--- js/index.js
+++ js/index.js
@@ -28,6 +28,62 @@ $(function () {
 		}
 		event.preventDefault();
 		event.stopPropagation();
-	})
+	});
+
+	$('#train-form').submit(function(e) {
+		e.preventDefault();
+		var formData = new FormData($('#train-form')[0]);
+
+		$.ajax({
+			type: 'POST',
+			url: '/v1/train',
+			data: formData,
+			contentType: false,
+			processData: false
+		}).done(function(data, textStatus, jqXHR) {
+			var d = JSON.parse(data);
+			if(d.status === 'true') {
+				$('#train-que-txt').val('');
+				$('#train-ans-txt').val('');
+				swal("Good job!", "Training is done!", "success");
+			}
+			else {
+				swal("Oooops!", "Training is unsuccessful!", "error");
+			}
+
+		}).fail(function(jqXHR, textStatus, errorThrown) {
+			console.log("Training failed");
+			swal("Oooops!", "Training is unsuccessful!", "error");
+		});
+	});
+
+	$('#evaluate-form').submit(function(e) {
+		e.preventDefault();
+		var formData = new FormData($('#evaluate-form')[0]);
+
+		$.ajax({
+			type: 'POST',
+			url: '/v1/evaluate',
+			data: formData,
+			contentType: false,
+			processData: false
+		}).done(function(data, textStatus, jqXHR) {
+			var d = JSON.parse(data);
+			if(d.status === 'true'){
+				$('#eval-ans-txt').text(d.ans);
+				$('#score-tag').show();
+				$('#score').text(d.score);
+				console.log('Evaluation success');
+			}
+			else{
+				$('#eval-ans-txt').text('Sorry, I could not understand.');
+				console.log(d.ans);
+			}
+		}).fail(function(jqXHR, textStatus, errorThrown) {
+			$('#eval-ans-txt').text('Sorry, I could not understand.');
+			console.log(d.ans);
+		});
+	});
+
     
 });
